b2949cacab4bcb31b657106ee86c5908105b0097
richard-zheng <1919500872@qq.com>
2020-03-20 17:03:09 +0800

    turn dialog activity to normal activity

--- app/src/main/java/com/richard/setyourpassword/WipeDataConfirmActivity.java
+++ app/src/main/java/com/richard/setyourpassword/WipeDataConfirmActivity.java
@@ -2,13 +2,37 @@ package com.richard.setyourpassword;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class WipeDataConfirmActivity extends AppCompatActivity {
+
+    public DevicePolicyManager dpm;
+    public ComponentName mDeviceAdminSample;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wipe_data_confirm);
+
+        dpm = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+        mDeviceAdminSample = new ComponentName(getApplicationContext(), MyAdmin.class);
+    }
+
+    public void wipeDataConfirm (View view) {
+        //获取文本框内容
+        EditText editText =(EditText)findViewById(R.id.editTextConfirm);
+        String str1 = editText.getText().toString();
+
+        //检查是否为yes
+        if (str1.equals("yes")) {
+            dpm.wipeData(1);
+        } else {
+            Toast.makeText(WipeDataConfirmActivity.this, R.string.toast_confirm_wipe_failed, Toast.LENGTH_SHORT).show();
+        }
     }
 }
