61bdcdb6353b2f91ec781d32a5cafcb02208f8a4
Richard Zheng <yuqiu36@live.com>
2020-11-03 21:58:39 +0800

    test action

--- app/src/main/java/com/richard/setyourpassword/AboutActivity.java
+++ app/src/main/java/com/richard/setyourpassword/AboutActivity.java
@@ -32,7 +32,6 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
     }
 
 }
